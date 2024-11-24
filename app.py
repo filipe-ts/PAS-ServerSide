5c00e2d6861534e62f67be418024f83cc63df402
Saulo <saulobenicio2016@gmail.com>
2024-11-24 10:57:35 -0300

    criacao do enpoint que retorna os filmes do catalogo

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 import requests
 import pathlib
 import os
 from dotenv import load_dotenv
-import json
+from urllib.parse import quote
 
 basedir = pathlib.Path(__file__).parent.resolve()
 load_dotenv(os.path.join(basedir, '.env'))
 
 key = os.getenv("OMDB_KEY")
+TMDB_TOKEN = os.getenv("TMDB_TOKEN")
 
 app = Flask(__name__)
 
@@ -23,3 +24,23 @@ def search(title):
     url = f"http://www.omdbapi.com/?t={title}&apikey={key}"
     response = requests.get(url=url)
     return f"<p>{response.text}</p>"
+
+@app.route("/movies/top-rated")
+def get_top_rated_movies():
+    url = "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1"
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {TMDB_TOKEN}"
+    }
+    
+    response = requests.get(url, headers=headers).json()
+    
+    if 'results' in response:
+        movies = response['results']
+        movie_list = [{"title": movie['title'], "overview": movie['overview'], "release_date": movie['release_date']} for movie in movies]
+        return jsonify(movie_list)
+    else:
+        return "<p>Erro ao obter filmes.</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
