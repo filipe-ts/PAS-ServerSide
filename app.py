0274644fbd4bf847ac4e40311e3d7312cfbaf632
Saulo <saulobenicio2016@gmail.com>
2024-11-24 14:48:18 -0300

    criacao da busca por filmes

--- app.py
+++ app.py
@@ -19,15 +19,45 @@ def get_title(title):
     response = json.loads(requests.get(url=url).text)
     return f"<p>{response["Title"]}</p>"
 
-@app.route("/search/<string:title>")
-def search(title):
-    url = f"http://www.omdbapi.com/?t={title}&apikey={key}"
-    response = requests.get(url=url)
-    return f"<p>{response.text}</p>"
+@app.route("/movies/search/<string:title>")
+def search(title): 
+    url = f"https://api.themoviedb.org/3/search/movie?query={title}&include_adult=false&language=pt-Br&page=1"
+    
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {TMDB_TOKEN}"
+    }
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        if 'results' in data:
+            movies = data['results']
+            movie_list = []
+            for movie in movies:
+                poster_url = f"https://image.tmdb.org/t/p/w500{movie['poster_path']}" if movie.get('poster_path') else None
+                backdrop_url = f"https://image.tmdb.org/t/p/w500{movie['backdrop_path']}" if movie.get('backdrop_path') else None
+                
+                movie_data = {
+                    "title": movie['title'],
+                    "overview": movie['overview'],
+                    "release_date": movie['release_date'],
+                    "vote_average": movie['vote_average'],
+                    "vote_count": movie['vote_count'],
+                    "poster_url": poster_url,
+                    "backdrop_url": backdrop_url,
+                }
+                movie_list.append(movie_data)
+            return jsonify(movie_list)
+        else:
+            return ""
+    else:
+        return ""
 
 @app.route("/movies/top-rated")
 def get_top_rated_movies():
-    url = "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1"
+    url = "https://api.themoviedb.org/3/movie/top_rated?language=pt-Br&page=1"
     headers = {
         "accept": "application/json",
         "Authorization": f"Bearer {TMDB_TOKEN}"
@@ -37,7 +67,21 @@ def get_top_rated_movies():
     
     if 'results' in response:
         movies = response['results']
-        movie_list = [{"title": movie['title'], "overview": movie['overview'], "release_date": movie['release_date']} for movie in movies]
+        movie_list = []
+        for movie in movies:
+            poster_url = f"https://image.tmdb.org/t/p/w500{movie['poster_path']}" if movie.get('poster_path') else None
+            
+            backdrop_url = f"https://image.tmdb.org/t/p/w500{movie['backdrop_path']}" if movie.get('backdrop_path') else None
+            
+            movie_data = {
+                "title": movie['title'],
+                "overview": movie['overview'],
+                "release_date": movie['release_date'],
+                "poster_url": poster_url,
+                "backdrop_url": backdrop_url
+            }
+            movie_list.append(movie_data)
+        
         return jsonify(movie_list)
     else:
         return "<p>Erro ao obter filmes.</p>"
